ea5068f675a895c53a41ff6625cb4aca079ac59a
Edgar <120991424+junerks@users.noreply.github.com>
2025-08-01 12:49:34 -0400

    Made footer | TODO: add responsive design to the page

--- src/app/services/pages/home/home.service.ts
+++ src/app/services/pages/home/home.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class HomeService {
   private http = inject(HttpClient);
   private url =
-    'http://localhost:1337/api/home?populate[0]=header.logo&populate[1]=header.button&populate[2]=banner&populate[3]=banner.image&populate[4]=banner.logo';
+    'http://localhost:1337/api/home?populate[0]=header.logo&populate[1]=header.button&populate[2]=banner&populate[3]=banner.image&populate[4]=banner.logo&populate[5]=footer&populate[6]=footer.logo&populate[7]=footer.contactInfo&&populate[8]=footer.contactInfo.contacts&populate[9]=footer.contactInfo.contacts.icon&populate[10]=footer.contactInfo.contacts.icon';
 
   fetch(): Observable<any> {
     return this.http.get<any>(this.url);
